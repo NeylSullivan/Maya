ce445c3a16eefbb96d27c56eec21f45f5ca3fce5
Neyl <neyl@list.ru>
2019-08-28 20:42:42 +0300

    Update DAZtoUE4_OptimizeSkeleton.py

--- python/DAZtoUE4_OptimizeSkeleton.py
+++ python/DAZtoUE4_OptimizeSkeleton.py
@@ -1,5 +1,124 @@
 import maya.cmds as cmds
 
+def RenameSkeletonJoints():
+    print 'Renaming skeleton joints'
+       
+    rename('hip', 'Hips')
+    rename('abdomenLower', 'Spine_1')
+    rename('abdomenUpper', 'Spine_2')
+    rename('chestLower', 'Spine_3')
+    rename('chestUpper', 'Spine_4')
+
+    rename('neckLower', 'Neck_1')
+    rename('neckUpper', 'Neck_2')
+    rename('head', 'Head')
+
+    rename('lPectoral', 'Pectoral_L')
+    rename('rPectoral', 'Pectoral_R')
+    #Arm Left
+    rename('lCollar', 'Clavicle_L')
+    rename('lShldrBend', 'ArmTop_L')
+    rename('lShldrTwist', 'ArmBottom_L')
+
+    rename('lForearmBend', 'ForeArmTop_L')
+    rename('lForearmTwist', 'ForeArmBottom_L')
+    rename('lHand', 'Hand_L')
+
+    rename('lThumb1', 'HandThumb1_L')
+    rename('lThumb2', 'HandThumb2_L')
+    rename('lThumb3', 'HandThumb3_L')
+
+    rename('lCarpal1', 'HandIndex0_L')
+    rename('lIndex1', 'HandIndex1_L')
+    rename('lIndex2', 'HandIndex2_L')
+    rename('lIndex3', 'HandIndex3_L')
+
+    rename('lCarpal2', 'HandMid0_L')
+    rename('lMid1', 'HandMid1_L')
+    rename('lMid2', 'HandMid2_L')
+    rename('lMid3', 'HandMid3_L')
+
+    rename('lCarpal3', 'HandRing0_L')
+    rename('lRing1', 'HandRing1_L')
+    rename('lRing2', 'HandRing2_L')
+    rename('lRing3', 'HandRing3_L')
+
+    rename('lCarpal4', 'HandPinky0_L')
+    rename('lPinky1', 'HandPinky1_L')
+    rename('lPinky2', 'HandPinky2_L')
+    rename('lPinky3', 'HandPinky3_L')
+
+    #Arm Right
+    rename('rCollar', 'Clavicle_R')
+    rename('rShldrBend', 'ArmTop_R')
+    rename('rShldrTwist', 'ArmBottom_R')
+
+    rename('rForearmBend', 'ForeArmTop_R')
+    rename('rForearmTwist', 'ForeArmBottom_R')
+    rename('rHand', 'Hand_R')
+
+    rename('rThumb1', 'HandThumb1_R')
+    rename('rThumb2', 'HandThumb2_R')
+    rename('rThumb3', 'HandThumb3_R')
+
+    rename('rCarpal1', 'HandIndex0_R')
+    rename('rIndex1', 'HandIndex1_R')
+    rename('rIndex2', 'HandIndex2_R')
+    rename('rIndex3', 'HandIndex3_R')
+
+    rename('rCarpal2', 'HandMid0_R')
+    rename('rMid1', 'HandMid1_R')
+    rename('rMid2', 'HandMid2_R')
+    rename('rMid3', 'HandMid3_R')
+
+    rename('rCarpal3', 'HandRing0_R')
+    rename('rRing1', 'HandRing1_R')
+    rename('rRing2', 'HandRing2_R')
+    rename('rRing3', 'HandRing3_R')
+
+    rename('rCarpal4', 'HandPinky0_R')
+    rename('rPinky1', 'HandPinky1_R')
+    rename('rPinky2', 'HandPinky2_R')
+    rename('rPinky3', 'HandPinky3_R')
+
+    #Leg Left
+    rename('lThighBend', 'UpLeg_L')
+    rename('lThighTwist', 'UpLegTwist_L')
+    rename('lShin', 'Leg_L')
+    rename('lFoot', 'Foot_L')
+    #rename('lMetatarsals', 'Metatarsals_L')
+    rename('lToe', 'Toe_L')
+
+    #Leg Right
+    rename('rThighBend', 'UpLeg_R')
+    rename('rThighTwist', 'UpLegTwist_R')
+    rename('rShin', 'Leg_R')
+    rename('rFoot', 'Foot_R')
+    #rename('rMetatarsals', 'Metatarsals_R')
+    rename('rToe', 'Toe_R')
+
+
+    #Face
+    rename('lEye', 'Eye_L')
+    rename('rEye', 'Eye_R')
+    rename('lEar', 'Ear_L')
+    rename('rEar', 'Ear_R')
+    rename('upperTeeth', 'UpperTeeth')
+    rename('lowerJaw', 'LowerJaw')
+    rename('lowerTeeth', 'LowerTeeth')
+    rename('tongue01', 'Tongue_1')
+    rename('tongue02', 'Tongue_2')
+    rename('tongue03', 'Tongue_3')
+    rename('tongue04', 'Tongue_4')
+
+    rename('lowerFaceRig', 'LowerFaceRig')
+    renameChildren('LowerFaceRig')
+
+    rename('upperFaceRig', 'UpperFaceRig')
+    renameChildren('UpperFaceRig')
+
+    #Root
+    rename('Genesis8Female', 'Root')
 
 def GetAverageWorldTranslationOfSelected():
     sel = cmds.ls(sl=True, fl=True)
@@ -80,9 +199,6 @@ def rotate (jointName, x=0, y=0, z=0):
         cmds.rotate( x, y, z, relative=True, objectSpace=True)
     except Exception:
         print('Это что ещё такое?')
-
-tempList = cmds.ls('DAZ_*', type="joint")
-cmds.delete(tempList)
 
 def rename (oldName, newName):
     try:
@@ -192,6 +308,174 @@ def DestroyJointChildren (jointName):
 
     print 'DestroyJointChildren: Done'
 
+    
+def DuplicateSkeletonJoints():
+    print 'Duplicating skeleton'
+
+    jointsList = cmds.ls(type="joint")
+
+    for j in jointsList:
+        pos = cmds.joint(j, q=True, absolute=True)
+        oldName = cmds.joint(j, q=True, name=True)
+        oldOrientation = cmds.joint(j, q=True, orientation=True)
+
+        newName = "DAZ_" + oldName
+        
+        cmds.select(clear=True) 
+        newJoint = cmds.joint(p=pos, name=newName)
+        cmds.xform( r=True, ro=oldOrientation )
+
+        
+def FixNewJointsOrientation():
+    print 'Fixing joint orientation'
+        
+    #Root
+    rotate("DAZ_Root", 90, 0, 90)
+        
+    #Spine
+    rotate("DAZ_Hips", 90, 0, 90)
+    rotate("DAZ_Spine_1", 90, 0, 90)
+    rotate("DAZ_Spine_2", 90, 0, 90)
+    rotate("DAZ_Spine_3", 90, 0, 90)
+    rotate("DAZ_Spine_4", 90, 0, 90)
+
+    rotate("DAZ_Pectoral_L", 90, 0, 90)
+    rotate("DAZ_Pectoral_R", 90, 0, 90)
+
+    rotate("DAZ_Neck_1", 90, 0, 90)
+    rotate("DAZ_Neck_2", 90, 0, 90)
+    rotate("DAZ_Head", 90, 0, 90)
+
+    #Leg Left
+    rotate("DAZ_UpLeg_L", 90, 0, -90)
+    rotate("DAZ_UpLegTwist_L", 90, 0, -90)
+    rotate("DAZ_Leg_L", 90, 0, -90)
+    #copy rotation from Leg
+    cmds.xform( 'DAZ_Foot_L', absolute=True, rotation=cmds.xform( 'DAZ_Leg_L', q=True, absolute=True, rotation=True ) )
+    rotate("DAZ_Toe_L", 0, -90, 0)
+
+    #Leg Right
+    rotate("DAZ_UpLeg_R", 90, 0, -90)
+    rotate("DAZ_UpLegTwist_R", 90, 0, -90)
+    rotate("DAZ_Leg_R", 90, 0, -90)
+    #copy rotation from Leg
+    cmds.xform( 'DAZ_Foot_R', absolute=True, rotation=cmds.xform( 'DAZ_Leg_R', q=True, absolute=True, rotation=True ) )
+    rotate("DAZ_Toe_R", 0, -90, 0)
+
+    #Arm Left
+
+    rotate("DAZ_Clavicle_L", 90)
+    rotate("DAZ_ArmTop_L", 90)
+    rotate("DAZ_ArmBottom_L", 90)
+    rotate("DAZ_ForeArmTop_L", 90)
+    rotate("DAZ_ForeArmBottom_L", 90)
+    rotate("DAZ_Hand_L", 90)
+
+    rotate('DAZ_HandThumb1_L', 180)
+    rotate('DAZ_HandThumb2_L', 180)
+    rotate('DAZ_HandThumb3_L', 180)
+
+    rotate('DAZ_HandIndex0_L', 90)
+    rotate('DAZ_HandIndex1_L', 90)
+    rotate('DAZ_HandIndex2_L', 90)
+    rotate('DAZ_HandIndex3_L', 90)
+
+    rotate('DAZ_HandMid0_L', 90)
+    rotate('DAZ_HandMid1_L', 90)
+    rotate('DAZ_HandMid2_L', 90)
+    rotate('DAZ_HandMid3_L', 90)
+
+    rotate('DAZ_HandRing0_L', 90)
+    rotate('DAZ_HandRing1_L', 90)
+    rotate('DAZ_HandRing2_L', 90)
+    rotate('DAZ_HandRing3_L', 90)
+
+    rotate('DAZ_HandPinky0_L', 90)
+    rotate('DAZ_HandPinky1_L', 90)
+    rotate('DAZ_HandPinky2_L', 90)
+    rotate('DAZ_HandPinky3_L', 90)
+
+    #Arm Right
+
+    rotate("DAZ_Clavicle_R",  -90, 180)
+    rotate("DAZ_ArmTop_R",  -90, 180)
+    rotate("DAZ_ArmBottom_R",  -90, 180)
+    rotate("DAZ_ForeArmTop_R",  -90, 180)
+    rotate("DAZ_ForeArmBottom_R",  -90, 180)
+    rotate("DAZ_Hand_R",  -90, 180)
+
+    rotate('DAZ_HandThumb1_R',  -180, 180)
+    rotate('DAZ_HandThumb2_R',  -180, 180)
+    rotate('DAZ_HandThumb3_R',  -180, 180)
+
+    rotate('DAZ_HandIndex0_R',  -90, 180)
+    rotate('DAZ_HandIndex1_R',  -90, 180)
+    rotate('DAZ_HandIndex2_R',  -90, 180)
+    rotate('DAZ_HandIndex3_R',  -90, 180)
+
+    rotate('DAZ_HandMid0_R',  -90, 180)
+    rotate('DAZ_HandMid1_R',  -90, 180)
+    rotate('DAZ_HandMid2_R',  -90, 180)
+    rotate('DAZ_HandMid3_R',  -90, 180)
+
+    rotate('DAZ_HandRing0_R',  -90, 180)
+    rotate('DAZ_HandRing1_R',  -90, 180)
+    rotate('DAZ_HandRing2_R',  -90, 180)
+    rotate('DAZ_HandRing3_R',  -90, 180)
+
+    rotate('DAZ_HandPinky0_R',  -90, 180)
+    rotate('DAZ_HandPinky1_R',  -90, 180)
+    rotate('DAZ_HandPinky2_R',  -90, 180)
+    rotate('DAZ_HandPinky3_R',  -90, 180)
+
+    #facial rig
+    rotate("DAZ_Tongue_1", 0, -90, 0)
+    rotate("DAZ_Tongue_2", 0, -90, 0)
+    rotate("DAZ_Tongue_3", 0, -90, 0)
+    rotate("DAZ_Tongue_4", 0, -90, 0)
+
+    children = cmds.listRelatives ('Head', allDescendents=True) #selecting original joints
+
+    for child in children:
+        if child.startswith('Tongue_'):
+            continue #skip
+        rotate('DAZ_' + child, 90, 0, 90) #but rotating skeleton copy
+
+
+    cmds.select(clear=True) 
+
+def RecreateHierarchy():
+    print 'Recreating hierarchy'
+
+    jointsList = cmds.listRelatives ('Root', allDescendents=True, type="joint")
+    #jointsList.append ('Root') #Root is already unparrented
+        
+    for j in jointsList:
+        parent = cmds.listRelatives( j, parent=True, type='joint' )
+        if not parent:
+            continue
+        oldName = cmds.joint(j, q=True, name=True)
+        oldParentName = cmds.joint(parent, q=True, name=True)
+        
+        newName = "DAZ_" + oldName
+        newParentName = "DAZ_" + oldParentName
+        #print newParentName
+        cmds.parent(newName, newParentName)
+    
+def RenameNewSkeleton():
+    print 'Renaming new skeleton'
+    newRoot = cmds.ls('DAZ_Root', type="joint")
+    newJoints = cmds.listRelatives( newRoot, allDescendents=True)
+    newJoints.append(newRoot[0])
+
+    for j in newJoints:
+        newName = j[4:]
+        print newName
+        rename(j, newName)
+
+#tempList = cmds.ls('DAZ_*', type="joint")
+#cmds.delete(tempList)
+    
 DestroyMiddleJoint ('lMetatarsals')
 DestroyMiddleJoint ('rMetatarsals')
 DestroyMiddleJoint ('pelvis')
@@ -199,275 +483,18 @@ DestroyJointChildren ('lToe')
 DestroyJointChildren ('rToe')
 DestroyJointChildren ('lSmallToe2_2')
 
+#set skinning type to linear
 skinList = cmds.ls(type='skinCluster')
-
 for s in skinList:
    cmds.skinCluster(s, e=True, skinMethod=0)
 
-print 'Renaming skeleton joints'
-   
-rename('hip', 'Hips')
-rename('abdomenLower', 'Spine_1')
-rename('abdomenUpper', 'Spine_2')
-rename('chestLower', 'Spine_3')
-rename('chestUpper', 'Spine_4')
+RenameSkeletonJoints()
 
-rename('neckLower', 'Neck_1')
-rename('neckUpper', 'Neck_2')
-rename('head', 'Head')
+DuplicateSkeletonJoints()
 
-rename('lPectoral', 'Pectoral_L')
-rename('rPectoral', 'Pectoral_R')
-#Arm Left
-rename('lCollar', 'Clavicle_L')
-rename('lShldrBend', 'ArmTop_L')
-rename('lShldrTwist', 'ArmBottom_L')
+FixNewJointsOrientation()
 
-rename('lForearmBend', 'ForeArmTop_L')
-rename('lForearmTwist', 'ForeArmBottom_L')
-rename('lHand', 'Hand_L')
-
-rename('lThumb1', 'HandThumb1_L')
-rename('lThumb2', 'HandThumb2_L')
-rename('lThumb3', 'HandThumb3_L')
-
-rename('lCarpal1', 'HandIndex0_L')
-rename('lIndex1', 'HandIndex1_L')
-rename('lIndex2', 'HandIndex2_L')
-rename('lIndex3', 'HandIndex3_L')
-
-rename('lCarpal2', 'HandMid0_L')
-rename('lMid1', 'HandMid1_L')
-rename('lMid2', 'HandMid2_L')
-rename('lMid3', 'HandMid3_L')
-
-rename('lCarpal3', 'HandRing0_L')
-rename('lRing1', 'HandRing1_L')
-rename('lRing2', 'HandRing2_L')
-rename('lRing3', 'HandRing3_L')
-
-rename('lCarpal4', 'HandPinky0_L')
-rename('lPinky1', 'HandPinky1_L')
-rename('lPinky2', 'HandPinky2_L')
-rename('lPinky3', 'HandPinky3_L')
-
-#Arm Right
-rename('rCollar', 'Clavicle_R')
-rename('rShldrBend', 'ArmTop_R')
-rename('rShldrTwist', 'ArmBottom_R')
-
-rename('rForearmBend', 'ForeArmTop_R')
-rename('rForearmTwist', 'ForeArmBottom_R')
-rename('rHand', 'Hand_R')
-
-rename('rThumb1', 'HandThumb1_R')
-rename('rThumb2', 'HandThumb2_R')
-rename('rThumb3', 'HandThumb3_R')
-
-rename('rCarpal1', 'HandIndex0_R')
-rename('rIndex1', 'HandIndex1_R')
-rename('rIndex2', 'HandIndex2_R')
-rename('rIndex3', 'HandIndex3_R')
-
-rename('rCarpal2', 'HandMid0_R')
-rename('rMid1', 'HandMid1_R')
-rename('rMid2', 'HandMid2_R')
-rename('rMid3', 'HandMid3_R')
-
-rename('rCarpal3', 'HandRing0_R')
-rename('rRing1', 'HandRing1_R')
-rename('rRing2', 'HandRing2_R')
-rename('rRing3', 'HandRing3_R')
-
-rename('rCarpal4', 'HandPinky0_R')
-rename('rPinky1', 'HandPinky1_R')
-rename('rPinky2', 'HandPinky2_R')
-rename('rPinky3', 'HandPinky3_R')
-
-#Leg Left
-rename('lThighBend', 'UpLeg_L')
-rename('lThighTwist', 'UpLegTwist_L')
-rename('lShin', 'Leg_L')
-rename('lFoot', 'Foot_L')
-#rename('lMetatarsals', 'Metatarsals_L')
-rename('lToe', 'Toe_L')
-
-#Leg Right
-rename('rThighBend', 'UpLeg_R')
-rename('rThighTwist', 'UpLegTwist_R')
-rename('rShin', 'Leg_R')
-rename('rFoot', 'Foot_R')
-#rename('rMetatarsals', 'Metatarsals_R')
-rename('rToe', 'Toe_R')
-
-
-#Face
-rename('lEye', 'Eye_L')
-rename('rEye', 'Eye_R')
-rename('lEar', 'Ear_L')
-rename('rEar', 'Ear_R')
-rename('upperTeeth', 'UpperTeeth')
-rename('lowerJaw', 'LowerJaw')
-rename('lowerTeeth', 'LowerTeeth')
-rename('tongue01', 'Tongue_1')
-rename('tongue02', 'Tongue_2')
-rename('tongue03', 'Tongue_3')
-rename('tongue04', 'Tongue_4')
-
-rename('lowerFaceRig', 'LowerFaceRig')
-renameChildren('LowerFaceRig')
-
-rename('upperFaceRig', 'UpperFaceRig')
-renameChildren('UpperFaceRig')
-
-#Root
-rename('Genesis8Female', 'Root')
-
-print 'Duplicating skeleton'
-
-jointsList = cmds.ls(type="joint")
-
-for j in jointsList:
-    pos = cmds.joint(j, q=True, absolute=True)
-    oldName = cmds.joint(j, q=True, name=True)
-    oldOrientation = cmds.joint(j, q=True, orientation=True)
-
-    newName = "DAZ_" + oldName
-    
-    cmds.select(clear=True) 
-    newJoint = cmds.joint(p=pos, name=newName)
-    cmds.xform( r=True, ro=oldOrientation )
-    
-print 'Fixing joint orientation'
-    
-#Root
-rotate("DAZ_Root", 90, 0, 90)
-    
-#Spine
-rotate("DAZ_Hips", 90, 0, 90)
-rotate("DAZ_Spine_1", 90, 0, 90)
-rotate("DAZ_Spine_2", 90, 0, 90)
-rotate("DAZ_Spine_3", 90, 0, 90)
-rotate("DAZ_Spine_4", 90, 0, 90)
-
-rotate("DAZ_Pectoral_L", 90, 0, 90)
-rotate("DAZ_Pectoral_R", 90, 0, 90)
-
-rotate("DAZ_Neck_1", 90, 0, 90)
-rotate("DAZ_Neck_2", 90, 0, 90)
-rotate("DAZ_Head", 90, 0, 90)
-
-#Leg Left
-rotate("DAZ_UpLeg_L", 90, 0, -90)
-rotate("DAZ_UpLegTwist_L", 90, 0, -90)
-rotate("DAZ_Leg_L", 90, 0, -90)
-#copy rotation from Leg
-cmds.xform( 'DAZ_Foot_L', absolute=True, rotation=cmds.xform( 'DAZ_Leg_L', q=True, absolute=True, rotation=True ) )
-rotate("DAZ_Toe_L", 0, -90, 0)
-
-#Leg Right
-rotate("DAZ_UpLeg_R", 90, 0, -90)
-rotate("DAZ_UpLegTwist_R", 90, 0, -90)
-rotate("DAZ_Leg_R", 90, 0, -90)
-#copy rotation from Leg
-cmds.xform( 'DAZ_Foot_R', absolute=True, rotation=cmds.xform( 'DAZ_Leg_R', q=True, absolute=True, rotation=True ) )
-rotate("DAZ_Toe_R", 0, -90, 0)
-
-#Arm Left
-
-rotate("DAZ_Clavicle_L", 90)
-rotate("DAZ_ArmTop_L", 90)
-rotate("DAZ_ArmBottom_L", 90)
-rotate("DAZ_ForeArmTop_L", 90)
-rotate("DAZ_ForeArmBottom_L", 90)
-rotate("DAZ_Hand_L", 90)
-
-rotate('DAZ_HandThumb1_L', 180)
-rotate('DAZ_HandThumb2_L', 180)
-rotate('DAZ_HandThumb3_L', 180)
-
-rotate('DAZ_HandIndex0_L', 90)
-rotate('DAZ_HandIndex1_L', 90)
-rotate('DAZ_HandIndex2_L', 90)
-rotate('DAZ_HandIndex3_L', 90)
-
-rotate('DAZ_HandMid0_L', 90)
-rotate('DAZ_HandMid1_L', 90)
-rotate('DAZ_HandMid2_L', 90)
-rotate('DAZ_HandMid3_L', 90)
-
-rotate('DAZ_HandRing0_L', 90)
-rotate('DAZ_HandRing1_L', 90)
-rotate('DAZ_HandRing2_L', 90)
-rotate('DAZ_HandRing3_L', 90)
-
-rotate('DAZ_HandPinky0_L', 90)
-rotate('DAZ_HandPinky1_L', 90)
-rotate('DAZ_HandPinky2_L', 90)
-rotate('DAZ_HandPinky3_L', 90)
-
-#Arm Right
-
-rotate("DAZ_Clavicle_R",  -90, 180)
-rotate("DAZ_ArmTop_R",  -90, 180)
-rotate("DAZ_ArmBottom_R",  -90, 180)
-rotate("DAZ_ForeArmTop_R",  -90, 180)
-rotate("DAZ_ForeArmBottom_R",  -90, 180)
-rotate("DAZ_Hand_R",  -90, 180)
-
-rotate('DAZ_HandThumb1_R',  -180, 180)
-rotate('DAZ_HandThumb2_R',  -180, 180)
-rotate('DAZ_HandThumb3_R',  -180, 180)
-
-rotate('DAZ_HandIndex0_R',  -90, 180)
-rotate('DAZ_HandIndex1_R',  -90, 180)
-rotate('DAZ_HandIndex2_R',  -90, 180)
-rotate('DAZ_HandIndex3_R',  -90, 180)
-
-rotate('DAZ_HandMid0_R',  -90, 180)
-rotate('DAZ_HandMid1_R',  -90, 180)
-rotate('DAZ_HandMid2_R',  -90, 180)
-rotate('DAZ_HandMid3_R',  -90, 180)
-
-rotate('DAZ_HandRing0_R',  -90, 180)
-rotate('DAZ_HandRing1_R',  -90, 180)
-rotate('DAZ_HandRing2_R',  -90, 180)
-rotate('DAZ_HandRing3_R',  -90, 180)
-
-rotate('DAZ_HandPinky0_R',  -90, 180)
-rotate('DAZ_HandPinky1_R',  -90, 180)
-rotate('DAZ_HandPinky2_R',  -90, 180)
-rotate('DAZ_HandPinky3_R',  -90, 180)
-
-#facial rig
-rotate("DAZ_Tongue_1", 0, -90, 0)
-rotate("DAZ_Tongue_2", 0, -90, 0)
-rotate("DAZ_Tongue_3", 0, -90, 0)
-rotate("DAZ_Tongue_4", 0, -90, 0)
-
-children = cmds.listRelatives ('Head', allDescendents=True) #selecting original joints
-
-for child in children:
-    if child.startswith('Tongue_'):
-        continue #skip
-    rotate('DAZ_' + child, 90, 0, 90) #but rotating skeleton copy
-
-
-cmds.select(clear=True) 
-
-print 'Recreating hierarchy'
-
-for j in jointsList:
-    parent = cmds.listRelatives( j, parent=True, type='joint' )
-    if not parent:
-        continue
-    oldName = cmds.joint(j, q=True, name=True)
-    oldParentName = cmds.joint(parent, q=True, name=True)
-    
-    newName = "DAZ_" + oldName
-    newParentName = "DAZ_" + oldParentName
-    #print newParentName
-    cmds.parent(newName, newParentName)
+RecreateHierarchy()
 
 print 'Parenting geometry to world'
 
@@ -479,9 +506,6 @@ for mesh in meshes:
         if cmds.listRelatives( tf, parent=True):
             cmds.parent(tf, world=True)
             
-            
-
-
 oldRoot = cmds.ls('Root', type="joint")
 oldJoints = cmds.listRelatives( oldRoot, allDescendents=True)
 oldJoints.append(oldRoot[0])
@@ -537,15 +561,8 @@ print 'TransferJointWeights: Done'
 print 'Deleting old skeleton'
 cmds.delete(oldJoints)
 
-print 'Renaming new skeleton'
-newRoot = cmds.ls('DAZ_Root', type="joint")
-newJoints = cmds.listRelatives( newRoot, allDescendents=True)
-newJoints.append(newRoot[0])
 
-for j in newJoints:
-    newName = j[4:]
-    print newName
-    rename(j, newName)
+RenameNewSkeleton()
 
 AddEndJoints()
 AddCameraJoint()
